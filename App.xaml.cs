0e9344c43ef8da63e22d4470f5f51f898e5e6f76
Thomas Haukland <thomas.haukland@gmail.com>
2024-10-21 16:33:42 +0200

    Initial MAUI template, deleted all but Android

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FunkyAsyncAnimations;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
